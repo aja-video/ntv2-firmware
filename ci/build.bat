6cacc13d51b1fac66cc85d77746342855d6d2b08
Thomas Greenman <thomasg@aja.com>
2023-06-05 15:03:11 -0700

    Add ci/build.bat script

---
+++ ci/build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set SELF_DIR=%~dp0
+set CHECKOUT_DIR=%SELF_DIR%..
+
+set IP_FIRMWARE_DIR=%CHECKOUT_DIR%\ip_firmware
+set SDK_DIR=%CHECKOUT_DIR%\oem_sdk
+
+REM Print artifact dependencies
+dir /s /b %IP_FIRMWARE_DIR%
+dir /s /b %SDK_DIR%
+
+REM Unzip all ip_firmware components
+for /f "delims=" %%f in ('dir /b /s *.zip') do (
+	"C:\Program Files\7-Zip\7z.exe" x -o"%%~dpI" "%%I"
+	if not %ERRORLEVEL% == 0 (
+		echo Error unzipping %%f
+		exit 1
+	)
+)
+
+dir /s /b %IP_FIRMWARE_DIR%
+
+REM Create all srecords files for each bitfile
+
