9ca5f07c8e475b107a569fc74d2fc62897bd53aa
Thomas Greenman <thomasg@aja.com>
2023-06-05 11:59:52 -0700

    Not going to use env.SystemPath for python dir. Should be in PATH

--- utility_files/create_corvid44_12g_srec.bat
+++ utility_files/create_corvid44_12g_srec.bat
@@ -2,4 +2,4 @@ mkdir srecords
 .\bin2srec -q -s -a 4 ..\corvid44-12g\tprom\c44_12g_8k_tprom.bit > .\srecords\corvid44_12g_8k.rec
 .\bin2srec -q -s -a 4 -o 2000000 ..\corvid44-12g\failsafe\c44_12g_8k_fs.bit >> .\srecords\corvid44_12g_8k.rec
 copy ..\corvid44-12g\tprom\c44_12g_8k_tprom.bit .\srecords\corvid44_12g_8k_tprom.bit
-%env.SystemDrive%\Python310\python readdate_rename.py srecords\corvid44_12g_8k_tprom.bit srecords\corvid44_12g_8k.rec
+python readdate_rename.py srecords\corvid44_12g_8k_tprom.bit srecords\corvid44_12g_8k.rec
