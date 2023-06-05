d43028752e49e5f76227422baf3c68b801d47978
Thomas Greenman <thomasg@aja.com>
2023-06-05 16:12:30 -0700

    Fix Kona5 script

--- utility_files/create_kona5_srec.bat
+++ utility_files/create_kona5_srec.bat
@@ -2,4 +2,4 @@ mkdir srecords
 .\bin2srec -q -s -a 4 ..\kona5\tprom\kona5_retail_tprom.bit > .\srecords\kona_5.rec
 .\bin2srec -q -s -a 4 -o 2000000 ..\kona5\failsafe\kona5_fs.bit >> .\srecords\kona_5.rec
 copy ..\kona5\tprom\kona5_retail_tprom.bit .\srecords\kona_5_retail_tprom.bit
-python readdate_rename.py srecords\kona_5\kona_5_retail_tprom.bit srecords\kona_5.rec
+python readdate_rename.py srecords\kona_5_retail_tprom.bit srecords\kona_5.rec
