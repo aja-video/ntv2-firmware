9893f6d02415202bd6c45fd328b1e35bf8d7bfb6
Thomas Greenman <thomasg@aja.com>
2023-06-05 15:45:26 -0700

    Fix paths in create_ioip and create_konaip scripts

--- utility_files/create_ioip2110_srec.bat
+++ utility_files/create_ioip2110_srec.bat
@@ -3,10 +3,10 @@ mkdir srecords
 set FIRMWARE_DIR=..\..\ip_firmware
 set IOIP_DIR=%FIRMWARE_DIR%\ioip-s2110
 
-powershell "type %IOIP_DIR%\mcs_components_2110\build7.h | format-hex > %IOIP_DIR%\mcs_components_2110\build7.bin"
-.\bin2srec -q -s -a 4 %IOIP_DIR%\mcs_components_2110\ioip_s2110.bit > .\srecords\IoIP_2110_Main.rec
-.\bin2srec -q -s -a 4 -o 2000000 %IOIP_DIR%\mcs_components_2110\ioip_s2110.bit >> .\srecords\IoIP_2110_Main.rec
-.\bin2srec -q -s -a 4 %IOIP_DIR%\mcs_components_2110\u-boot-s.bin >> .\srecords\IoIP_2110_Micro.rec
-.\bin2srec -q -s -a 4 -o 100000 %IOIP_DIR%\mcs_components_2110\image.ub >> .\srecords\IoIP_2110_Micro.rec
-.\bin2srec -q -s -a 4 -o 1f40000 %IOIP_DIR%\mcs_components_2110\build7.bin >> .\srecords\IoIP_2110_Micro.rec
-copy %IOIP_DIR%\mcs_components_2110\ioip_s2110.bit .\srecords\IoIP_2110.bit
+powershell "type %IOIP_DIR%\build7.h | format-hex > %IOIP_DIR%\build7.bin"
+.\bin2srec -q -s -a 4 %IOIP_DIR%\ioip_s2110.bit > .\srecords\IoIP_2110_Main.rec
+.\bin2srec -q -s -a 4 -o 2000000 %IOIP_DIR%\ioip_s2110.bit >> .\srecords\IoIP_2110_Main.rec
+.\bin2srec -q -s -a 4 %IOIP_DIR%\u-boot-s.bin >> .\srecords\IoIP_2110_Micro.rec
+.\bin2srec -q -s -a 4 -o 100000 %IOIP_DIR%\image.ub >> .\srecords\IoIP_2110_Micro.rec
+.\bin2srec -q -s -a 4 -o 1f40000 %IOIP_DIR%\build7.bin >> .\srecords\IoIP_2110_Micro.rec
+copy %IOIP_DIR%\ioip_s2110.bit .\srecords\IoIP_2110.bit
--- utility_files/create_kona2022_srec.bat
+++ utility_files/create_kona2022_srec.bat
@@ -3,11 +3,11 @@ mkdir srecords
 set FIRMWARE_DIR=..\..\ip_firmware
 set KIP_DIR=%FIRMWARE_DIR%\kip-s2022
 
-powershell "(gc %KIP_DIR%\mcs_components\build7.h | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:x2}' -f $_}) -join '' > %KIP_DIR%\mcs_components\build7.bin"
-.\bin2srec -q -s -a 4 %KIP_DIR%\mcs_components\s2022_56_4ch_rxtx.bit > .\srecords\konaIP_2022.rec
-.\bin2srec -q -s -a 4 %KIP_DIR%\mcs_components\s2022_56_4ch_rxtx.bit > .\srecords\konaIP_2022.rec
-.\bin2srec -q -s -a 4 -o 1000000 %KIP_DIR%\mcs_components\s2022_56_4ch_rxtx.bit >> .\srecords\konaIP_2022.rec
-.\bin2srec -q -s -a 4 -o 1f40000 %KIP_DIR%\mcs_components\build7.h >> .\srecords\konaIP_2022.rec
-.\bin2srec -q -s -a 4 -o 2000000 %KIP_DIR%\mcs_components\u-boot-s.bin >> .\srecords\konaIP_2022.rec
-.\bin2srec -q -s -a 4 -o 20c0000 %KIP_DIR%\mcs_components\image.ub >> .\srecords\konaIP_2022.rec
-copy %KIP_DIR%\mcs_components\s2022_56_4ch_rxtx.bit .\srecords\konaIP_2022.bit
+powershell "(gc %KIP_DIR%\build7.h | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '{0:x2}' -f $_}) -join '' > %KIP_DIR%\build7.bin"
+.\bin2srec -q -s -a 4 %KIP_DIR%\s2022_56_4ch_rxtx.bit > .\srecords\konaIP_2022.rec
+.\bin2srec -q -s -a 4 %KIP_DIR%\s2022_56_4ch_rxtx.bit > .\srecords\konaIP_2022.rec
+.\bin2srec -q -s -a 4 -o 1000000 %KIP_DIR%\s2022_56_4ch_rxtx.bit >> .\srecords\konaIP_2022.rec
+.\bin2srec -q -s -a 4 -o 1f40000 %KIP_DIR%\build7.h >> .\srecords\konaIP_2022.rec
+.\bin2srec -q -s -a 4 -o 2000000 %KIP_DIR%\u-boot-s.bin >> .\srecords\konaIP_2022.rec
+.\bin2srec -q -s -a 4 -o 20c0000 %KIP_DIR%\image.ub >> .\srecords\konaIP_2022.rec
+copy %KIP_DIR%\s2022_56_4ch_rxtx.bit .\srecords\konaIP_2022.bit
