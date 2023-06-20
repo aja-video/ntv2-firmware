ddfa4d277141a1d69c85a233b5b24f5d8a948cfa
Thomas Greenman <thomasg@aja.com>
2023-06-20 16:49:04 -0700

    ci: add tools dir

--- ci/build.bat
+++ ci/build.bat
@@ -23,6 +23,7 @@ if exist %STAGING_DIR% (
 mkdir %STAGING_DIR%
 mkdir %STAGING_DIR%\srecords
 mkdir %STAGING_DIR%\sdk
+mkdir %STAGING_DIR%\tools
 
 REM List artifact dependencies
 dir /s /b %IP_FIRMWARE_DIR%
@@ -30,6 +31,7 @@ dir /s /b %SDK_DIR%
 
 REM Unzip SDK to sdk directory
 xcopy /s /y %SDK_DIR%\ntv2sdk*.zip %STAGING_DIR%\sdk
+xcopy /s /y %SDK_DIR%\ntv2tools*.zip %STAGING_DIR%\tools
 
 REM Unzip all ip_firmware components
 chdir %IP_FIRMWARE_DIR%
